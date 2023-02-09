9c658613478707b2cd480c9d5758dee3333a9015
maulanafahrul <maulanafahrul2004@gmail.com>
2023-02-09 23:51:58 +0900

    add some feature view add todo

---
+++ testing/TestViewAddTodoList.php
@@ -0,0 +1,20 @@
+<?php 
+
+require_once "../view/ViewAddTodoList.php";
+require_once "../businessLogic/AddTodoList.php";
+require_once "../businessLogic/ShowTodoList.php";
+
+addTodoList("aaa");
+addTodoList("bbb");
+addTodoList("ccc");
+
+viewAddTodoList();
+
+showTodolist();
+
+viewAddTodoList();
+
+showTodolist();
+
+
+
--- view/ViewAddTodoList.php
+++ view/ViewAddTodoList.php
@@ -1,7 +1,18 @@
 <?php 
 
+require_once "../modal/TodoList.php";
+require_once "../businessLogic/AddTodoList.php";
+require_once "../helper/Input.php";
 
 function viewAddTodoList()
 {
+    echo "MENAMBAHKAN TODOLIST" . PHP_EOL;
 
+    $todo = input("Todo (Masukan x jika ingin membatalkan) ");
+
+    if ($todo == "x"|| $todo == "X") {
+        echo "Batal memasukan todo" . PHP_EOL;
+    }else {
+        addTodoList($todo);
+    }
 }
